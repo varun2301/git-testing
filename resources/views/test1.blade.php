da5d3e2a46681bbec73f8c230887abeb373c1572
Varun Kumar <varun.k@hestabit.in>
2018-02-13 15:16:03 +0530

    commit from master branch

--- resources/views/test1.blade.php
+++ resources/views/test1.blade.php
@@ -1,16 +1,3 @@
 <?php
-	echo "commit from another branch";
-
-	echo "2 commit from another branch";
-
-	echo "3 commit from another branch";	 
-
-    echo "commit from v2 branch";
-
-    echo "2 commit from v2 branch";
-
-    echo "3 commit from v2 branch";
-
-    echo "4 commit from v2 branch";
-
+	echo "code commit from master branch";
 ?>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,3 @@
 <?php
-    echo "commit from v2 branch";
-
-    echo "2 commit from v2 branch";
-
-    echo "3 commit from v2 branch";
+    echo "code commit from master branch";
 ?>

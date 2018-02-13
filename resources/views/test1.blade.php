943e141a2dc26da61544b56ec485395d8d09b325
Varun Kumar <varun.k@hestabit.in>
2018-02-13 15:33:48 +0530

    2 commit from local branch one system

--- resources/views/test1.blade.php
+++ resources/views/test1.blade.php
@@ -1,3 +1,4 @@
 <?php
 	echo "commit from one system";
+	echo "2 commit from one system";
 ?>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
 <?php
     echo "commit from one system local branch";
+    echo "2 commit from one system local branch";
 ?>
